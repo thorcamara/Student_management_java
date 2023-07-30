36394aaa2afe352a9fb2a976da2f3775e363e3ec
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-29 21:45:19 -0300

    Added Constructor.

--- src/StudentDatabaseApp/StudentDatabaseApp.java
+++ src/StudentDatabaseApp/StudentDatabaseApp.java
@@ -2,6 +2,7 @@ package StudentDatabaseApp;
 
 public class StudentDatabaseApp {
     public static void main(String[] args) {
+        Student stu1 = new Student();
         // Ask how many users we want to add
 
         // Create n number of students
