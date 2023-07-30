9ce3820930e0ff4781744128de60bd91ab5288fa
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-29 21:50:23 -0300

    Added Student ID.

--- src/StudentDatabaseApp/Student.java
+++ src/StudentDatabaseApp/Student.java
@@ -6,10 +6,11 @@ public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private int studentID;
+    private String studentID;
     private String courses;
     private int tuitionBalance;
-    private int costOfCourse = 600;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
 
     // Constructor: prompt user to enter student's name and year
     public Student() {
@@ -22,10 +23,19 @@ public class Student {
 
         System.out.print("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level: ");
         this.gradeYear = in.nextInt();
-        System.out.println(firstName + " " + lastName + " " + gradeYear);
+
+        setStudentID();
+
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
     }
 
     // Generate an ID
+    private void setStudentID() {
+        // Grade Level + ID
+        id++;
+        this.studentID =  gradeYear + "" + id;
+
+    }
 
     // View balance
 
