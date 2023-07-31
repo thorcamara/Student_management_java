406ba6834797bfb2aebf661e47a8b1ca4d4ac425
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-30 23:25:24 -0300

    Added an Array to be able to add multiple students.

--- src/StudentDatabaseApp/StudentDatabaseApp.java
+++ src/StudentDatabaseApp/StudentDatabaseApp.java
@@ -1,14 +1,25 @@
 package StudentDatabaseApp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.toString());
 
-        // Ask how many users we want to add
+        // Ask how many new students we want to add
+        System.out.print("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
 
         // Create n number of students
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+        }
+
+        for (int n = 0; n < numOfStudents; n++) {
+            System.out.println(students[n].toString());
+        }
     }
 }
